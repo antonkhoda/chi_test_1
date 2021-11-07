1ed89623d67f6fd048dc8791ddee420f47beeeaf
Anton Khoda <anton.khoda@chisw.com>
2021-11-07 20:58:28 +0200

    img fix

--- src/index.js
+++ src/index.js
@@ -2,26 +2,22 @@ import "./styles/main.scss";
 
 const hamburger = document.querySelector(".hamburger_box");
 const hamburger_body = document.querySelector(".hamburger_body");
-const hamburger_pic_on =
-    "url(../src/img/hamburger/hamburger-on.png) no-repeat center";
-const hamburger_pic_off =
-    "url(../src/img/hamburger/hamburger-off.png) no-repeat center";
 let check = false;
 
 window.addEventListener("resize", function () {
-    if (window.innerWidth <= 768) {
-        hamburger_body.style.display = "none";
-        hamburger.style.background = hamburger_pic_on;
-        hamburger.style.backgroundSize = "70%";
-        check = false;
-    } else {
-        hamburger_body.style.display = "flex";
-    }
+  if (window.innerWidth <= 768) {
+    hamburger_body.style.display = "none";
+    hamburger.classList.remove("hamburger_box_active");
+    check = false;
+  } else {
+    hamburger_body.style.display = "flex";
+  }
 });
 
 hamburger.addEventListener("click", function () {
-    check = !check;
-    this.style.background = check ? hamburger_pic_off : hamburger_pic_on;
-    hamburger_body.style.display = check ? "flex" : "none";
-    this.style.backgroundSize = "70%";
+  check = !check;
+  check
+    ? hamburger.classList.add("hamburger_box_active")
+    : hamburger.classList.remove("hamburger_box_active");
+  hamburger_body.style.display = check ? "flex" : "none";
 });
--- webpack.config.js
+++ webpack.config.js
@@ -20,16 +20,15 @@ module.exports = {
   ],
 
   module: {
-    rules: [
-        {
-            test: /\.s[ac]ss$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
+    rules: [{
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }
     ],
-},
+  },
 };
